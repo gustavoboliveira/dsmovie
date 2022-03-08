2b854483870d59f533bf3febe932e1db378fd4e7
Gustavo Oliveira <gustavo.oliveira@ilegra.com>
2022-03-08 15:27:45 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Listing } from 'pages/Listing/index'
+import { Form } from 'pages/Form/index'
 import { Navbar } from './components/Navbar/index'
 
 export const App = () => {
-  return <Navbar />
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+        <Route path="/" element={<Listing />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,3 @@
+export const Listing = () => {
+  return <h1>Página de listagem</h1>
+}
