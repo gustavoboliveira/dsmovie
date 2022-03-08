71abc77ab1099e06647f425f8ea19ad820a46366
Gustavo Oliveira <gustavo.oliveira@ilegra.com>
2022-03-08 17:08:59 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import './styles.css'
 
 export const Form = () => {
@@ -40,7 +42,9 @@ export const Form = () => {
             </button>
           </div>
         </form>
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        <Link to="/">
+          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </Link>
       </div>
     </div>
   )
