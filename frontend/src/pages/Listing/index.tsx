c5e9362e2e1bfac4697ba350690664ae8835e56f
Gustavo Oliveira <gustavo.oliveira@ilegra.com>
2022-03-11 17:03:57 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
+import axios from 'axios'
 import { MovieCard } from 'components/MovieCard'
 import { Pagination } from 'components/Pagination'
+import { BASE_URL } from 'utils/requests'
 
 export const Listing = () => {
+  //Forma Errada
+  axios.get(`${BASE_URL}/movies`).then((response) => {
+    console.log(response.data)
+  })
+
   return (
     <>
       <Pagination />
