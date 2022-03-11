8979d7374e3374e689e379b63373347149aca54b
Gustavo Oliveira <gustavo.oliveira@ilegra.com>
2022-03-11 17:12:37 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,13 +1,20 @@
 import axios from 'axios'
 import { MovieCard } from 'components/MovieCard'
 import { Pagination } from 'components/Pagination'
+import { useEffect, useState } from 'react'
+import { MoviePage } from 'types/movie'
 import { BASE_URL } from 'utils/requests'
 
 export const Listing = () => {
-  //Forma Errada
-  axios.get(`${BASE_URL}/movies`).then((response) => {
-    console.log(response.data)
-  })
+  const [pageNumber, setPageNumber] = useState(0)
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`).then((response) => {
+      const data = response.data as MoviePage
+      console.log(data)
+      setPageNumber(data.number)
+    })
+  }, [])
 
   return (
     <>
